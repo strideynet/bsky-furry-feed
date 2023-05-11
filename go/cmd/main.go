67bf73efe2dac5afa4afa2658f50ec52744db00c
Noah Stride <noah@noahstride.co.uk>
2023-05-12 00:40:03 +0100

    Parse the feed params

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -14,10 +14,14 @@ import (
 	"github.com/bluesky-social/indigo/repomgr"
 	"github.com/gorilla/websocket"
 	"github.com/oklog/run"
+	"go.opentelemetry.io/otel"
 	"golang.org/x/exp/slog"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+var tracer = otel.Tracer("github.com/strideynet/bsky-furry-feed")
 
 func main() {
 	log := slog.New(slog.NewTextHandler(os.Stderr, nil))
@@ -60,6 +64,9 @@ func fireHose(ctx context.Context, log *slog.Logger) error {
 
 	return events.HandleRepoStream(ctx, con, &events.RepoStreamCallbacks{
 		RepoCommit: func(evt *atproto.SyncSubscribeRepos_Commit) error {
+			ctx, span := tracer.Start(ctx, "FireHose.HandleRepoCommit")
+			defer span.End()
+
 			if evt.Repo != noahDid {
 				return nil
 			}
@@ -106,16 +113,36 @@ func fireHose(ctx context.Context, log *slog.Logger) error {
 	})
 }
 
+type getFeedSkeletonParameters struct {
+	cursor string
+	limit  int
+	feed   string
+}
+
 func feedServer(log *slog.Logger) *http.Server {
 	mux := &http.ServeMux{}
 	mux.Handle("/xrpc/app.bsky.feed.getFeedSkeleton", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		params := getFeedSkeletonParameters{
+			cursor: q.Get("cursor"),
+			feed:   q.Get("feed"),
+		}
+		limitStr := q.Get("limit")
+		if limitStr != "" {
+			limit, err := strconv.Atoi(limitStr)
+			if err != nil {
+				panic(err)
+			}
+			params.limit = limit
+		}
+
 		w.WriteHeader(200)
-		data := map[string]any{
+		output := map[string]any{
 			"cursor": "my-cursor",
 			"feed":   []any{},
 		}
 		encoder := json.NewEncoder(w)
-		encoder.Encode(data)
+		encoder.Encode(output)
 	}))
 	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Info("request")
