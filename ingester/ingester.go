1403e6fe3d9216b19bed3e82a49e2493836afe7f
Noah Stride <noah@noahstride.co.uk>
2023-05-17 19:36:37 +0100

    Tidy up concurrency management using an errgroup in Ingester

--- ingester/ingester.go
+++ ingester/ingester.go
@@ -21,6 +21,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 	"net/http"
 	"os"
 	"sync"
@@ -29,104 +30,120 @@ import (
 
 var tracer = otel.Tracer("github.com/strideynet/bsky-furry-feed/ingester")
 
-var eventsProcessed = promauto.NewSummaryVec(prometheus.SummaryOpts{
-	Name: "bff_ingester_firehose_event_duration_seconds",
-	Help: "The total number of events from the firehose processed by workers",
+var workItemsProcessed = promauto.NewSummaryVec(prometheus.SummaryOpts{
+	Name: "bff_ingester_work_item_duration_seconds",
+	Help: "The total number of work items handled by the ingester worker pool.",
 }, []string{"type"})
 
-const workerCount = 5
-
 type FirehoseIngester struct {
+	// dependencies
 	log     *zap.Logger
 	crc     *CandidateRepositoryCache
 	queries *store.Queries
+
+	// configuration
+	subscribeURL    string
+	workerCount     int
+	workItemTimeout time.Duration
 }
 
-func NewFirehoseIngester(log *zap.Logger, queries *store.Queries, crc *CandidateRepositoryCache) *FirehoseIngester {
+func NewFirehoseIngester(
+	log *zap.Logger, queries *store.Queries, crc *CandidateRepositoryCache,
+) *FirehoseIngester {
 	return &FirehoseIngester{
 		log:     log,
 		crc:     crc,
 		queries: queries,
+
+		subscribeURL:    "wss://bsky.social/xrpc/com.atproto.sync.subscribeRepos",
+		workerCount:     5,
+		workItemTimeout: time.Second * 30,
 	}
 }
 
-// TODO: Shutdown syncronisation of the various goroutines in this function
-// is a little rubbish - need to revisit this and fix it.
 func (fi *FirehoseIngester) Start(ctx context.Context) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	subscribeUrl := "wss://bsky.social/xrpc/com.atproto.sync.subscribeRepos"
-
-	con, _, err := websocket.DefaultDialer.Dial(subscribeUrl, http.Header{})
-	if err != nil {
-		return fmt.Errorf("dialing websocket: %w", err)
-	}
-
-	go func() {
-		<-ctx.Done()
-		fi.log.Info("stopping firehose ingester")
-		fi.log.Info("closing websocket connection")
-		if err := con.Close(); err != nil {
-			fi.log.Error(
-				"error occurred closing websocket",
-				zap.Error(err),
-			)
-		}
-		fi.log.Info("closed websocket")
-		cancel()
-	}()
+	eg, egCtx := errgroup.WithContext(ctx)
 
 	// Unbuffered channel so that the websocket will stop reading if the workers
-	// are not redy.
+	// are not ready. In future, we may want to consider some reasonable
+	// buffering to account for short spikes in event rates.
 	evtChan := make(chan *atproto.SyncSubscribeRepos_Commit)
-	workerWg := sync.WaitGroup{}
-	for workerN := 1; workerN < workerCount; workerN++ {
-		workerWg.Add(1)
+	eg.Go(func() error {
+		workerWg := sync.WaitGroup{}
+		for n := 1; n < fi.workerCount; n++ {
+			workerWg.Add(1)
+			go func() {
+				defer workerWg.Done()
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					case evt := <-evtChan:
+						// record start time so we can collect
+						start := time.Now()
+						// 30 seconds max to deal with any work item. This
+						// prevents a worker hanging.
+						ctx, cancel := context.WithTimeout(
+							ctx, fi.workItemTimeout,
+						)
+						if err := fi.handleCommit(ctx, evt); err != nil {
+							fi.log.Error(
+								"failed to handle repo commit",
+								zap.Error(err),
+							)
+						}
+						workItemsProcessed.
+							WithLabelValues("repo_commit").
+							Observe(time.Since(start).Seconds())
+						cancel()
+					}
+				}
+			}()
+		}
+		workerWg.Wait()
+		return nil
+	})
+
+	eg.Go(func() error {
+		con, _, err := websocket.DefaultDialer.DialContext(
+			egCtx, fi.subscribeURL, http.Header{},
+		)
+		if err != nil {
+			return fmt.Errorf("dialing websocket: %w", err)
+		}
+
 		go func() {
-			defer workerWg.Done()
-			for {
+			<-egCtx.Done()
+			fi.log.Info("closing websocket subscription")
+			if err := con.Close(); err != nil {
+				fi.log.Error(
+					"error occurred closing websocket",
+					zap.Error(err),
+				)
+			}
+			fi.log.Info("closed websocket subscription")
+		}()
+		// TODO: sometimes stream exits of own accord, we should attempt to
+		// reconnect several times and then return an error to cause the
+		// process to crash out.
+		return events.HandleRepoStream(egCtx, con, &events.RepoStreamCallbacks{
+			RepoCommit: func(evt *atproto.SyncSubscribeRepos_Commit) error {
 				select {
 				case <-ctx.Done():
-					return
-				case evt := <-evtChan:
-					start := time.Now()
-					// 30 seconds max to deal with any batch. This prevents a worker
-					// hanging.
-					ctx, cancel := context.WithTimeout(ctx, time.Second*30)
-					if err := fi.handleCommit(ctx, evt); err != nil {
-						fi.log.Error("failed to handle repo commit", zap.Error(err))
-					}
-					eventsProcessed.WithLabelValues("repo_commit").Observe(time.Since(start).Seconds())
-					cancel()
+					// Ensure we don't get stuck waiting for a worker even if
+					// the connection has shutdown.
+					return nil
+				case evtChan <- evt:
 				}
-			}
-		}()
-	}
-	// TODO: sometimes stream exits of own accord, lets recover from that.
-	err = events.HandleRepoStream(ctx, con, &events.RepoStreamCallbacks{
-		RepoCommit: func(evt *atproto.SyncSubscribeRepos_Commit) error {
-			select {
-			case <-ctx.Done():
-				// Ensure we don't get stuck waiting for a worker even if the
-				// server has shutdown.
 				return nil
-			case evtChan <- evt:
-			}
-			return nil
-		},
+			},
+		})
 	})
-	if err != nil {
-		return err
-	}
-	cancel()
-	fi.log.Info("waiting for workers to finish")
-	workerWg.Wait()
-	fi.log.Info("workers finished")
 
-	return err
+	return eg.Wait()
 }
 
-func (fi *FirehoseIngester) handleCommit(rootCtx context.Context, evt *atproto.SyncSubscribeRepos_Commit) error {
+func (fi *FirehoseIngester) handleCommit(ctx context.Context, evt *atproto.SyncSubscribeRepos_Commit) error {
 	// Dispose of events from non-candidate repositories
 	candidateUser := fi.crc.GetByDID(evt.Repo)
 	if candidateUser == nil {
@@ -135,7 +152,7 @@ func (fi *FirehoseIngester) handleCommit(rootCtx context.Context, evt *atproto.S
 	// TODO: Find a way to use tail-based sampling so that we can capture this trace
 	// before candidateUser is run and ensure we always capture candidateUser
 	// traces.
-	ctx, span := tracer.Start(rootCtx, "firehose_ingester.handle_commit")
+	ctx, span := tracer.Start(ctx, "firehose_ingester.handle_commit")
 	defer span.End()
 	span.SetAttributes(
 		attribute.String("candidate_repository.did", evt.Repo),
