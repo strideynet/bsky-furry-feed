e10d9d3f98f327e5eb2e0926da6d1335aa49a1b0
Noah Stride <noah@noahstride.co.uk>
2023-05-19 16:00:38 +0200

    Add candidate actors from replies to furry broadcast thread

--- cmd/bffctl/scan.go
+++ cmd/bffctl/scan.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/strideynet/bsky-furry-feed/bluesky"
 	"github.com/strideynet/bsky-furry-feed/store"
 	"github.com/urfave/cli/v2"
 	"go.uber.org/zap"
 	"golang.org/x/exp/slices"
 	"os"
+	"strings"
+	"time"
 )
 
 type stateFile struct {
@@ -28,8 +31,8 @@ func postRepliesScanSource(
 	client *bluesky.Client,
 	uri string,
 	excludeActors []string,
-) ([]string, error) {
-	prospectActors := []string{}
+) (map[string]struct{}, error) {
+	prospectActors := map[string]struct{}{}
 	thread, err := client.GetPostThread(ctx, uri)
 	if err != nil {
 		return nil, fmt.Errorf("getting post thread: %w", err)
@@ -41,10 +44,9 @@ func postRepliesScanSource(
 
 	for _, reply := range thread.Thread.FeedDefs_ThreadViewPost.Replies {
 		actor := reply.FeedDefs_ThreadViewPost.Post.Author.Did
-
 		// TODO: a map is going to be more performant here
 		if !slices.Contains(excludeActors, actor) {
-			prospectActors = append(prospectActors, actor)
+			prospectActors[actor] = struct{}{}
 		}
 	}
 
@@ -62,9 +64,9 @@ func scanCmd(log *zap.Logger, env *environment) *cli.Command {
 			}
 			defer conn.Close(cctx.Context)
 
-			store := store.New(conn)
+			queries := store.New(conn)
 
-			existingActors, err := store.ListCandidateActors(cctx.Context)
+			existingActors, err := queries.ListCandidateActors(cctx.Context)
 			if err != nil {
 				return fmt.Errorf("listing candidate actors: %w", err)
 			}
@@ -92,13 +94,74 @@ func scanCmd(log *zap.Logger, env *environment) *cli.Command {
 			}
 			log.Info("scan phase complete", zap.Int("found_count", len(prospectActors)))
 
-			for _, actor := range prospectActors {
+			for actor := range prospectActors {
 				profile, err := client.GetProfile(cctx.Context, actor)
 				if err != nil {
 					return fmt.Errorf("getting profile: %w, err")
 				}
-				fmt.Printf("%s (%s) - https://bsky.app/profile/%s\n", *profile.DisplayName, profile.Handle, profile.Did)
+
+				displayName := "none"
+				if profile.DisplayName != nil {
+					displayName = *profile.DisplayName
+				}
+				desc := "none"
+				if profile.Description != nil {
+					desc = *profile.Description
+				}
+
+				fmt.Printf("---\n%s (%s)\n", displayName, profile.Handle)
+				fmt.Printf("link: https://bsky.app/profile/%s\n", profile.Did)
+				fmt.Printf("desc:\n%s\n", desc)
+				fmt.Printf("(a)dd, (r)eject, (s)kip, (q)uit: ")
+				action := ""
+				_, err = fmt.Scanln(&action)
+				if err != nil {
+					return fmt.Errorf("scanning user input: %w", err)
+				}
+
+				switch strings.ToLower(action) {
+				case "skip", "s":
+					continue
+				case "quit", "q":
+					return nil
+				case "reject", "r":
+					fmt.Println("rejecting...")
+				case "add", "a":
+					params := store.CreateCandidateActorParams{
+						DID:     profile.Did,
+						Comment: fmt.Sprintf("%s (%s)", displayName, profile.Handle),
+						CreatedAt: pgtype.Timestamptz{
+							Time:  time.Now(),
+							Valid: true,
+						},
+					}
+					fmt.Printf("is this account an artist [y/n]: ")
+
+					isArtist := ""
+					_, err = fmt.Scanln(&isArtist)
+					if err != nil {
+						return fmt.Errorf("scanning user input: %w", err)
+					}
+					switch strings.ToLower(isArtist) {
+					case "y":
+						params.IsArtist = true
+					case "n":
+						params.IsArtist = false
+					default:
+						return fmt.Errorf("expected y or n but got %q", isArtist)
+					}
+
+					err := queries.CreateCandidateActor(cctx.Context, params)
+					if err != nil {
+						return fmt.Errorf("creating candidate actor: %w", err)
+					}
+
+					fmt.Println("successfully added")
+				default:
+					return fmt.Errorf("expected y or n but got %q", action)
+				}
 			}
+			log.Info("all prospective actors handled")
 
 			return nil
 		},
