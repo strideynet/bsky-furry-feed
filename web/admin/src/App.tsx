9dbdbace357220649a6d7128127145ad2745e82b
Noah Stride <noah@noahstride.co.uk>
2023-07-05 09:22:13 +0100

    Add initial hack uisngmui

--- web/admin/src/App.tsx
+++ web/admin/src/App.tsx
@@ -1,33 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useMemo, useState} from 'react'
+import {AppBskyActorDefs, AtpAgent} from "@atproto/api";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Grid,
+  Paper,
+  Card,
+  CardContent
+} from "@mui/material";
+import Typography from '@mui/material/Typography';
 
-function App() {
-  const [count, setCount] = useState(0)
+interface session {
+  did: string
+}
+
+const useSession = (agent: AtpAgent) => {
+  const [session, setSession] = useState<session | undefined>()
+
+  useEffect(() => {
+    agent.login({ identifier: import.meta.env.VITE_BSKY_USERNAME, password: import.meta.env.VITE_BSKY_PASSWORD })
+      .then((res) => {
+        setSession({
+          did: res.data.did,
+        })
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [agent])
+
+  return session
+}
+
+const useGetProfile = (agent: AtpAgent, session?: session) => {
+  const [profile, setProfile] = useState<AppBskyActorDefs.ProfileViewDetailed | undefined>()
+
+  useEffect(() => {
+    if (!session) {
+      console.debug("no session")
+      return
+    }
+
+    agent.api.app.bsky.actor.getProfile({ actor: session.did })
+      .then((res) => {
+        setProfile(res.data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [session])
+
+  return profile
+}
+
+type  ProfileComponentProps = {
+  profile: AppBskyActorDefs.ProfileViewDetailed,
+}
+
+const ProfileComponent = ({ profile }: ProfileComponentProps) => {
+  return <>
+    Name: { profile.displayName }<br/>
+    Handle: { profile.handle }
+  </>
+}
+
+const App = () => {
+  const agent = useMemo(() => {
+    return new AtpAgent({ service: 'https://bsky.social' })
+  }, [])
+  const session = useSession(agent)
+  const profile = useGetProfile(agent, session)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit" component="div">
+            Furryli.st
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={8} lg={4}>
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <Typography variant="h4">
+                  About You
+                </Typography>
+                <Typography variant="body1">
+                  {profile ?
+                    <ProfileComponent profile={profile}/>
+                    : 'loading'}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={8} lg={8}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
