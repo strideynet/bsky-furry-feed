e73563433507cd48b00f429f8deb0643ab5f2ae8
Noah Stride <noah@noahstride.co.uk>
2023-07-05 20:24:20 +0100

    Prettierify

--- web/admin/src/App.tsx
+++ web/admin/src/App.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useMemo, useState} from 'react'
-import {AppBskyActorDefs, AtpAgent} from "@atproto/api";
+import { useEffect, useMemo, useState } from "react";
+import { AppBskyActorDefs, AtpAgent } from "@atproto/api";
 import {
   ActionIcon,
   AppShell,
@@ -7,102 +7,119 @@ import {
   Group,
   Header,
   Image,
-  Navbar, rem,
+  Navbar,
+  rem,
   Text,
   Title,
-  useMantineColorScheme, useMantineTheme
+  useMantineColorScheme,
+  useMantineTheme,
 } from "@mantine/core";
-import {IconMoonStars, IconSun} from "@tabler/icons-react";
-import {Outlet} from "@tanstack/router";
+import { IconMoonStars, IconSun } from "@tabler/icons-react";
+import { Outlet } from "@tanstack/router";
 
 interface session {
-  did: string
+  did: string;
 }
 
 const useSession = (agent: AtpAgent) => {
-  const [session, setSession] = useState<session | undefined>()
+  const [session, setSession] = useState<session | undefined>();
 
   useEffect(() => {
-    agent.login({
-      identifier: import.meta.env.VITE_BSKY_USERNAME,
-      password: import.meta.env.VITE_BSKY_PASSWORD
-    })
+    agent
+      .login({
+        identifier: import.meta.env.VITE_BSKY_USERNAME,
+        password: import.meta.env.VITE_BSKY_PASSWORD,
+      })
       .then((res) => {
         setSession({
           did: res.data.did,
-        })
+        });
       })
       .catch((err) => {
-        console.error(err)
-      })
-  }, [agent])
+        console.error(err);
+      });
+  }, [agent]);
 
-  return session
-}
+  return session;
+};
 
 const useGetProfile = (agent: AtpAgent, session?: session) => {
-  const [profile, setProfile] = useState<AppBskyActorDefs.ProfileViewDetailed | undefined>()
+  const [profile, setProfile] = useState<
+    AppBskyActorDefs.ProfileViewDetailed | undefined
+  >();
 
   useEffect(() => {
     if (!session) {
-      console.debug("no session")
-      return
+      console.debug("no session");
+      return;
     }
 
-    agent.api.app.bsky.actor.getProfile({actor: session.did})
+    agent.api.app.bsky.actor
+      .getProfile({ actor: session.did })
       .then((res) => {
-        setProfile(res.data)
+        setProfile(res.data);
       })
       .catch((err) => {
-        console.error(err)
-      })
-  }, [session])
+        console.error(err);
+      });
+  }, [session]);
 
-  return profile
-}
+  return profile;
+};
 
 const App = () => {
-  const {colorScheme, toggleColorScheme} = useMantineColorScheme();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const theme = useMantineTheme();
 
   const agent = useMemo(() => {
-    return new AtpAgent({service: 'https://bsky.social'})
-  }, [])
-  const session = useSession(agent)
-  const profile = useGetProfile(agent, session)
+    return new AtpAgent({ service: "https://bsky.social" });
+  }, []);
+  const session = useSession(agent);
+  const profile = useGetProfile(agent, session);
 
   return (
     <>
       <AppShell
         padding="md"
         navbar={
-          <Navbar width={{base: 300}} p="xs">
+          <Navbar width={{ base: 300 }} p="xs">
             <Navbar.Section grow mt="xs">
               <Text>Approval Queue</Text>
             </Navbar.Section>
             <Navbar.Section>
-              <Box sx={{
-                borderTop: `${rem(1)} solid ${
-                  theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
-                }`
-              }}>
+              <Box
+                sx={{
+                  borderTop: `${rem(1)} solid ${
+                    theme.colorScheme === "dark"
+                      ? theme.colors.dark[4]
+                      : theme.colors.gray[2]
+                  }`,
+                }}
+              >
                 <Title order={3}>Your User</Title>
-                {profile ?
+                {profile ? (
                   <Text>
-                    Handle: {profile.handle}<br/>
+                    Handle: {profile.handle}
+                    <br />
                     Username: {profile.displayName}
-                  </Text> : <Text>Loading...</Text>
-                }
+                  </Text>
+                ) : (
+                  <Text>Loading...</Text>
+                )}
               </Box>
             </Navbar.Section>
           </Navbar>
         }
         header={
           <Header height={60} p="xs">
-            <Group sx={{height: '100%'}} px={20} position="apart">
+            <Group sx={{ height: "100%" }} px={20} position="apart">
               <Group>
-                <Image src="/dogmotif.png" radius="md" width={40}
-                       fit="contain"/>
+                <Image
+                  src="/dogmotif.png"
+                  radius="md"
+                  width={40}
+                  fit="contain"
+                />
                 <Title>Admin Dash</Title>
               </Group>
 
@@ -111,25 +128,32 @@ const App = () => {
                 onClick={() => toggleColorScheme()}
                 size={30}
               >
-                {colorScheme === 'dark' ? <IconSun size="1rem"/> :
-                  <IconMoonStars size="1rem"/>}
+                {colorScheme === "dark" ? (
+                  <IconSun size="1rem" />
+                ) : (
+                  <IconMoonStars size="1rem" />
+                )}
               </ActionIcon>
             </Group>
-
           </Header>
         }
         styles={(theme) => ({
-          main: {backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0]},
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
         })}
       >
-        < Outlet />
+        <Outlet />
       </AppShell>
     </>
-  )
-}
+  );
+};
 
 export const HomePage = () => {
-  return <Text>Home!</Text>
-}
+  return <Text>Home!</Text>;
+};
 
-export default App
+export default App;
--- web/admin/src/main.tsx
+++ web/admin/src/main.tsx
@@ -1,47 +1,48 @@
-import React, {useState} from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { RootRoute, Route, Router, RouterProvider } from "@tanstack/router";
 import {
-  RouterProvider,
-  Router,
-  Route,
-  RootRoute,
-} from '@tanstack/router'
-import {ColorScheme, ColorSchemeProvider, MantineProvider} from "@mantine/core";
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
 
-import App, {HomePage} from './App.tsx'
+import App, { HomePage } from "./App.tsx";
 
 // TODO: Pull routing into seperate file!
 const rootRoute = new RootRoute({
   component: App,
-})
+});
 
 const indexRoute = new Route({
   getParentRoute: () => rootRoute,
-  path: '/',
+  path: "/",
   component: HomePage,
-})
+});
 
-const routeTree = rootRoute.addChildren([indexRoute])
+const routeTree = rootRoute.addChildren([indexRoute]);
 
-const router = new Router({ routeTree })
+const router = new Router({ routeTree });
 
 const Root = () => {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
   const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
-  return <React.StrictMode>
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <RouterProvider router={router} />
-      </MantineProvider>
-    </ColorSchemeProvider>
-  </React.StrictMode>
-}
+  return (
+    <React.StrictMode>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <RouterProvider router={router} />
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </React.StrictMode>
+  );
+};
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Root/>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Root />,
+);
