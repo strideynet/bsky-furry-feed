967194cb6a34fd78607db5f4faa9be9211253fae
Noah Stride <noah@noahstride.co.uk>
2023-07-25 19:56:07 +0100

    Correc tite rname

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -19,7 +19,7 @@ resource "google_sql_database_instance" "main_us_east" {
     disk_autoresize   = true
     disk_size         = 15
     disk_type         = "PD_SSD"
-    tier              = "db-g1-micro"
+    tier              = "db-g1-small"
     deletion_protection_enabled = true
 
     backup_configuration {
