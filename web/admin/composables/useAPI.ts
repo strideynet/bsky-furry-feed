78c0790ba5c66d77714628e48bbc1ed545b9ceeb
Kev <kevslashnull@gmail.com>
2023-07-28 17:13:22 +0200

    Make admin API URL configurable in environment

--- web/admin/composables/useAPI.ts
+++ web/admin/composables/useAPI.ts
@@ -3,9 +3,10 @@ import { createConnectTransport } from "@bufbuild/connect-web";
 import { ModerationService } from "../../proto/bff/v1/moderation_service_connectweb";
 
 export default async function () {
+  const { apiUrl } = useRuntimeConfig().public;
   const user = await useUser();
   const transport = createConnectTransport({
-    baseUrl: "https://feed.furryli.st",
+    baseUrl: apiUrl,
 
     fetch(input, data = {}) {
       (data.headers as Headers).set(
