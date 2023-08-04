82bb02ca98e0f3b6190157fc5d3d040939c3a920
Noah Stride <noah@noahstride.co.uk>
2023-08-04 10:39:30 +0100

    Add auth test

--- api/moderation_auth_test.go
+++ api/moderation_auth_test.go
@@ -8,7 +8,9 @@ import (
 	v1 "github.com/strideynet/bsky-furry-feed/proto/bff/v1"
 	"go.uber.org/zap/zaptest"
 	"google.golang.org/protobuf/proto"
+	"reflect"
 	"testing"
+	"unsafe"
 )
 
 type memoryActorGetter map[string]*v1.Actor
@@ -22,43 +24,99 @@ func (mag memoryActorGetter) GetActorByDID(_ context.Context, did string) (*v1.A
 	return proto.Clone(v).(*v1.Actor), nil
 }
 
+func setSpec(req connect.AnyRequest, spec connect.Spec) {
+	specVal := reflect.ValueOf(req).
+		Elem().FieldByName("spec")
+
+	reflect.NewAt(
+		specVal.Type(),
+		unsafe.Pointer(specVal.UnsafeAddr())).Elem().Set(reflect.ValueOf(spec))
+}
+
 func TestAuthEngine(t *testing.T) {
 	tests := []struct {
 		name string
 
-		actorGetter   actorGetter
+		headerKey     string
+		headerValue   string
+		actor         *v1.Actor
 		procedureName string
 
 		want    *authContext
 		wantErr string
 	}{
 		{
-			name: "success",
-			actorGetter: memoryActorGetter{
-				"exists": &v1.Actor{
-					Did:   "exists",
-					Roles: []string{"admin"},
-				},
+			name:          "success",
+			headerKey:     "Authorization",
+			headerValue:   "Bearer exists",
+			procedureName: "/bff.v1.ModerationService/CreateActor",
+			actor: &v1.Actor{
+				Did:   "exists",
+				Roles: []string{"admin"},
 			},
+			want: &authContext{
+				DID: "exists",
+			},
+		},
+		{
+			name:          "no header",
+			procedureName: "/bff.v1.ModerationService/CreateActor",
+			actor: &v1.Actor{
+				Did:   "exists",
+				Roles: []string{"admin"},
+			},
+			wantErr: "unauthenticated: no token provided",
+		},
+		{
+			name:          "malformed header",
+			headerKey:     "Authorization",
+			headerValue:   "rewgwegnmwerogkmowergiopwergiopwergop",
+			procedureName: "/bff.v1.ModerationService/CreateActor",
+			actor: &v1.Actor{
+				Did:   "exists",
+				Roles: []string{"admin"},
+			},
+			wantErr: "unauthenticated: malformed header",
+		},
+		{
+			name:          "unsupported auth type",
+			headerKey:     "Authorization",
+			headerValue:   "OtherType foo",
+			procedureName: "/bff.v1.ModerationService/CreateActor",
+			actor: &v1.Actor{
+				Did:   "exists",
+				Roles: []string{"admin"},
+			},
+			wantErr: "unauthenticated: only Bearer auth supported",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ctx := context.Background()
+			mag := memoryActorGetter{}
+			if tt.actor != nil {
+				mag[tt.actor.Did] = tt.actor
+			}
 			ae := &AuthEngine{
-				ActorGetter: tt.actorGetter,
-				// TODO: Inject TokenValidator fake
+				ActorGetter: mag,
+				TokenValidator: func(ctx context.Context, token string) (did string, err error) {
+					return token, nil
+				},
 				Log: zaptest.NewLogger(t),
 			}
 
 			req := connect.NewRequest(&v1.PingRequest{})
-			// TODO: figure out how to inject a procedureName
+			if tt.headerKey != "" {
+				req.Header().Set(tt.headerKey, tt.headerValue)
+			}
+			setSpec(req, connect.Spec{Procedure: tt.procedureName})
 			got, err := ae.auth(ctx, req)
 			if tt.wantErr != "" {
 				require.EqualError(t, err, tt.wantErr)
 				return
 			}
+			require.NoError(t, err)
 			require.Equal(t, tt.want, got)
 		})
 	}
