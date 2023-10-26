db62e7b28babbde83221ff23c640d43696aa1ee5
Kev <kevslashnull@gmail.com>
2023-10-27 01:33:10 +0200

    Fix duplicate session requests

--- web/admin/composables/useUser.ts
+++ web/admin/composables/useUser.ts
@@ -5,9 +5,17 @@ export default async function (): Promise<Ref<AtpSessionData>> {
   const user = useState("user");
   const session = useCookie(COOKIE_NAME).value;
 
-  if (session) {
+  if (session && !user.value) {
     user.value = await fetchUser();
   }
 
   return user as Ref<AtpSessionData>;
 }
+
+/**
+ * 6.23
+12 XHR requests
+
+3.76
+7 XHR requests
+ */
