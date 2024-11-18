4ba33a4f55cc48a2a105320bc1a10655be75a4b8
Kev <kevslashnull@gmail.com>
2024-11-18 23:06:55 +0100

    Speed up queries in prod

--- store/gen/candidate_posts.sql.go
+++ store/gen/candidate_posts.sql.go
@@ -58,91 +58,94 @@ func (q *Queries) CreateCandidatePost(ctx context.Context, arg CreateCandidatePo
 
 const getFurryNewFeed = `-- name: GetFurryNewFeed :many
 WITH args AS (
-    SELECT $7::TEXT [] AS allowed_embeds
+    SELECT $1::TEXT [] AS allowed_embeds
+),
+
+candidate_posts AS (
+    SELECT cp.uri, cp.actor_did, cp.created_at, cp.indexed_at, cp.is_hidden, cp.deleted_at, cp.raw, cp.hashtags, cp.has_media, cp.self_labels, cp.has_video
+    FROM
+        candidate_posts AS cp
+    INNER JOIN candidate_actors AS ca ON cp.actor_did = ca.did
+    NATURAL JOIN args
+    WHERE
+        -- Only include posts by approved actors
+        ca.status = 'approved'
+        -- Remove posts hidden by our moderators
+        AND cp.is_hidden = FALSE
+        -- Remove posts deleted by the actors
+        AND cp.deleted_at IS NULL
+        AND (
+        -- Standard criteria.
+            (
+                -- Match at least one of the queried hashtags.
+                -- If unspecified, do not filter.
+                (
+                    COALESCE($2::TEXT [], '{}') = '{}'
+                    OR $2::TEXT [] && cp.hashtags
+                )
+                -- If any hashtags are disallowed, filter them out.
+                AND (
+                    COALESCE($3::TEXT [], '{}') = '{}'
+                    OR NOT $3::TEXT [] && cp.hashtags
+                )
+                AND (
+                    CARDINALITY(args.allowed_embeds) = 0
+                    OR (
+                        'none' = ANY(args.allowed_embeds)
+                        AND COALESCE(cp.has_media, FALSE) = FALSE
+                        AND COALESCE(cp.has_video, FALSE) = FALSE
+                    )
+                    OR (
+                        'image' = ANY(args.allowed_embeds)
+                        AND COALESCE(cp.has_media, FALSE) = TRUE
+                    )
+                    OR (
+                        'video' = ANY(args.allowed_embeds)
+                        AND COALESCE(cp.has_video, FALSE) = TRUE
+                    )
+                )
+                -- Filter by NSFW status. If unspecified, do not filter.
+                AND (
+                    $4::BOOLEAN IS NULL
+                    OR (
+                        (ARRAY['nsfw', 'mursuit', 'murrsuit', 'nsfwfurry', 'furrynsfw'] && cp.hashtags)
+                        OR (ARRAY['porn', 'nudity', 'sexual'] && cp.self_labels)
+                    ) = $4
+                )
+            )
+            -- Pinned DID criteria.
+            OR cp.actor_did = ANY($5::TEXT [])
+        )
+        -- Remove posts newer than the cursor timestamp
+        AND (cp.indexed_at < $6)
+        AND cp.indexed_at > NOW() - INTERVAL '7 day'
+    ORDER BY
+        cp.indexed_at DESC
+    LIMIT $7
 )
 
-SELECT cp.uri, cp.actor_did, cp.created_at, cp.indexed_at, cp.is_hidden, cp.deleted_at, cp.raw, cp.hashtags, cp.has_media, cp.self_labels, cp.has_video
-FROM
-    candidate_posts AS cp
-INNER JOIN candidate_actors AS ca ON cp.actor_did = ca.did
-NATURAL JOIN args
-WHERE
-    -- Only include posts by approved actors
-    ca.status = 'approved'
-    -- Remove posts hidden by our moderators
-    AND cp.is_hidden = FALSE
-    -- Remove posts deleted by the actors
-    AND cp.deleted_at IS NULL
-    AND (
-    -- Standard criteria.
-        (
-            -- Match at least one of the queried hashtags.
-            -- If unspecified, do not filter.
-            (
-                COALESCE($1::TEXT [], '{}') = '{}'
-                OR $1::TEXT [] && cp.hashtags
-            )
-            -- If any hashtags are disallowed, filter them out.
-            AND (
-                COALESCE($2::TEXT [], '{}') = '{}'
-                OR NOT $2::TEXT [] && cp.hashtags
-            )
-            AND (
-                CARDINALITY(args.allowed_embeds) = 0
-                OR (
-                    'none' = ANY(args.allowed_embeds)
-                    AND COALESCE(cp.has_media, FALSE) = FALSE
-                    AND COALESCE(cp.has_video, FALSE) = FALSE
-                )
-                OR (
-                    'image' = ANY(args.allowed_embeds)
-                    AND COALESCE(cp.has_media, FALSE) = TRUE
-                )
-                OR (
-                    'video' = ANY(args.allowed_embeds)
-                    AND COALESCE(cp.has_video, FALSE) = TRUE
-                )
-            )
-            -- Filter by NSFW status. If unspecified, do not filter.
-            AND (
-                $3::BOOLEAN IS NULL
-                OR (
-                    (ARRAY['nsfw', 'mursuit', 'murrsuit', 'nsfwfurry', 'furrynsfw'] && cp.hashtags)
-                    OR (ARRAY['porn', 'nudity', 'sexual'] && cp.self_labels)
-                ) = $3
-            )
-        )
-        -- Pinned DID criteria.
-        OR cp.actor_did = ANY($4::TEXT [])
-    )
-    -- Remove posts newer than the cursor timestamp
-    AND (cp.indexed_at < $5)
-    AND cp.indexed_at > NOW() - INTERVAL '7 day'
-    AND cp.created_at > NOW() - INTERVAL '7 day'
-ORDER BY
-    cp.indexed_at DESC
-LIMIT $6
+SELECT uri, actor_did, created_at, indexed_at, is_hidden, deleted_at, raw, hashtags, has_media, self_labels, has_video FROM candidate_posts WHERE created_at > NOW() - INTERVAL '7 day'
 `
 
 type GetFurryNewFeedParams struct {
+	AllowedEmbeds      []string
 	Hashtags           []string
 	DisallowedHashtags []string
 	IsNSFW             pgtype.Bool
 	PinnedDIDs         []string
 	CursorTimestamp    pgtype.Timestamptz
 	Limit              int32
-	AllowedEmbeds      []string
 }
 
 func (q *Queries) GetFurryNewFeed(ctx context.Context, arg GetFurryNewFeedParams) ([]CandidatePost, error) {
 	rows, err := q.db.Query(ctx, getFurryNewFeed,
+		arg.AllowedEmbeds,
 		arg.Hashtags,
 		arg.DisallowedHashtags,
 		arg.IsNSFW,
 		arg.PinnedDIDs,
 		arg.CursorTimestamp,
 		arg.Limit,
-		arg.AllowedEmbeds,
 	)
 	if err != nil {
 		return nil, err
@@ -204,71 +207,75 @@ func (q *Queries) GetPostByURI(ctx context.Context, uri string) (CandidatePost, 
 
 const listScoredPosts = `-- name: ListScoredPosts :many
 WITH args AS (
-    SELECT $9::TEXT [] AS allowed_embeds
+    SELECT $1::TEXT [] AS allowed_embeds
+),
+
+candidate_posts AS (
+    SELECT
+        cp.uri, cp.actor_did, cp.created_at, cp.indexed_at, cp.is_hidden, cp.deleted_at, cp.raw, cp.hashtags, cp.has_media, cp.self_labels, cp.has_video, cp.score,
+        ph.score
+    FROM
+        candidate_posts AS cp
+    INNER JOIN candidate_actors AS ca ON cp.actor_did = ca.did
+    INNER JOIN post_scores AS ph
+        ON
+            cp.uri = ph.uri AND ph.alg = $2
+            AND ph.generation_seq = $3
+    NATURAL JOIN args
+    WHERE
+        cp.is_hidden = FALSE
+        AND ca.status = 'approved'
+        -- Match at least one of the queried hashtags.
+        -- If unspecified, do not filter.
+        AND (
+            COALESCE($4::TEXT [], '{}') = '{}'
+            OR $4::TEXT [] && cp.hashtags
+        )
+        -- If any hashtags are disallowed, filter them out.
+        AND (
+            COALESCE($5::TEXT [], '{}') = '{}'
+            OR NOT $5::TEXT [] && cp.hashtags
+        )
+        AND (
+            CARDINALITY(args.allowed_embeds) = 0
+            OR (
+                'none' = ANY(args.allowed_embeds)
+                AND COALESCE(cp.has_media, FALSE) = FALSE
+                AND COALESCE(cp.has_video, FALSE) = FALSE
+            )
+            OR (
+                'image' = ANY(args.allowed_embeds)
+                AND COALESCE(cp.has_media, FALSE) = TRUE
+            )
+            OR (
+                'video' = ANY(args.allowed_embeds)
+                AND COALESCE(cp.has_video, FALSE) = TRUE
+            )
+        )
+        -- Filter by NSFW status. If unspecified, do not filter.
+        AND (
+            $6::BOOLEAN IS NULL
+            OR (
+                (ARRAY['nsfw', 'mursuit', 'murrsuit', 'nsfwfurry', 'furrynsfw'] && cp.hashtags)
+                OR (ARRAY['porn', 'nudity', 'sexual'] && cp.self_labels)
+            ) = $6
+        )
+        AND cp.deleted_at IS NULL
+        AND (
+            ROW(ph.score, ph.uri)
+            < ROW(($7)::REAL, ($8)::TEXT)
+        )
+        AND cp.indexed_at > NOW() - INTERVAL '7 day'
+    ORDER BY
+        ph.score DESC, ph.uri DESC
+    LIMIT $9
 )
 
-SELECT
-    cp.uri, cp.actor_did, cp.created_at, cp.indexed_at, cp.is_hidden, cp.deleted_at, cp.raw, cp.hashtags, cp.has_media, cp.self_labels, cp.has_video,
-    ph.score
-FROM
-    candidate_posts AS cp
-INNER JOIN candidate_actors AS ca ON cp.actor_did = ca.did
-INNER JOIN post_scores AS ph
-    ON
-        cp.uri = ph.uri AND ph.alg = $1
-        AND ph.generation_seq = $2
-NATURAL JOIN args
-WHERE
-    cp.is_hidden = FALSE
-    AND ca.status = 'approved'
-    -- Match at least one of the queried hashtags.
-    -- If unspecified, do not filter.
-    AND (
-        COALESCE($3::TEXT [], '{}') = '{}'
-        OR $3::TEXT [] && cp.hashtags
-    )
-    -- If any hashtags are disallowed, filter them out.
-    AND (
-        COALESCE($4::TEXT [], '{}') = '{}'
-        OR NOT $4::TEXT [] && cp.hashtags
-    )
-    AND (
-        CARDINALITY(args.allowed_embeds) = 0
-        OR (
-            'none' = ANY(args.allowed_embeds)
-            AND COALESCE(cp.has_media, FALSE) = FALSE
-            AND COALESCE(cp.has_video, FALSE) = FALSE
-        )
-        OR (
-            'image' = ANY(args.allowed_embeds)
-            AND COALESCE(cp.has_media, FALSE) = TRUE
-        )
-        OR (
-            'video' = ANY(args.allowed_embeds)
-            AND COALESCE(cp.has_video, FALSE) = TRUE
-        )
-    )
-    -- Filter by NSFW status. If unspecified, do not filter.
-    AND (
-        $5::BOOLEAN IS NULL
-        OR (
-            (ARRAY['nsfw', 'mursuit', 'murrsuit', 'nsfwfurry', 'furrynsfw'] && cp.hashtags)
-            OR (ARRAY['porn', 'nudity', 'sexual'] && cp.self_labels)
-        ) = $5
-    )
-    AND cp.deleted_at IS NULL
-    AND (
-        ROW(ph.score, ph.uri)
-        < ROW(($6)::REAL, ($7)::TEXT)
-    )
-    AND cp.indexed_at > NOW() - INTERVAL '7 day'
-    AND cp.created_at > NOW() - INTERVAL '7 day'
-ORDER BY
-    ph.score DESC, ph.uri DESC
-LIMIT $8
+SELECT uri, actor_did, created_at, indexed_at, is_hidden, deleted_at, raw, hashtags, has_media, self_labels, has_video, score FROM candidate_posts WHERE created_at > NOW() - INTERVAL '7 day'
 `
 
 type ListScoredPostsParams struct {
+	AllowedEmbeds      []string
 	Alg                string
 	GenerationSeq      int64
 	Hashtags           []string
@@ -277,7 +284,6 @@ type ListScoredPostsParams struct {
 	AfterScore         float32
 	AfterURI           string
 	Limit              int32
-	AllowedEmbeds      []string
 }
 
 type ListScoredPostsRow struct {
@@ -297,6 +303,7 @@ type ListScoredPostsRow struct {
 
 func (q *Queries) ListScoredPosts(ctx context.Context, arg ListScoredPostsParams) ([]ListScoredPostsRow, error) {
 	rows, err := q.db.Query(ctx, listScoredPosts,
+		arg.AllowedEmbeds,
 		arg.Alg,
 		arg.GenerationSeq,
 		arg.Hashtags,
@@ -305,7 +312,6 @@ func (q *Queries) ListScoredPosts(ctx context.Context, arg ListScoredPostsParams
 		arg.AfterScore,
 		arg.AfterURI,
 		arg.Limit,
-		arg.AllowedEmbeds,
 	)
 	if err != nil {
 		return nil, err
--- store/queries/candidate_posts.sql
+++ store/queries/candidate_posts.sql
@@ -25,69 +25,72 @@ WHERE
 -- name: GetFurryNewFeed :many
 WITH args AS (
     SELECT sqlc.narg(allowed_embeds)::TEXT [] AS allowed_embeds
+),
+
+candidate_posts AS (
+    SELECT cp.*
+    FROM
+        candidate_posts AS cp
+    INNER JOIN candidate_actors AS ca ON cp.actor_did = ca.did
+    NATURAL JOIN args
+    WHERE
+        -- Only include posts by approved actors
+        ca.status = 'approved'
+        -- Remove posts hidden by our moderators
+        AND cp.is_hidden = FALSE
+        -- Remove posts deleted by the actors
+        AND cp.deleted_at IS NULL
+        AND (
+        -- Standard criteria.
+            (
+                -- Match at least one of the queried hashtags.
+                -- If unspecified, do not filter.
+                (
+                    COALESCE(sqlc.narg(hashtags)::TEXT [], '{}') = '{}'
+                    OR sqlc.arg(hashtags)::TEXT [] && cp.hashtags
+                )
+                -- If any hashtags are disallowed, filter them out.
+                AND (
+                    COALESCE(sqlc.narg(disallowed_hashtags)::TEXT [], '{}') = '{}'
+                    OR NOT sqlc.narg(disallowed_hashtags)::TEXT [] && cp.hashtags
+                )
+                AND (
+                    CARDINALITY(args.allowed_embeds) = 0
+                    OR (
+                        'none' = ANY(args.allowed_embeds)
+                        AND COALESCE(cp.has_media, FALSE) = FALSE
+                        AND COALESCE(cp.has_video, FALSE) = FALSE
+                    )
+                    OR (
+                        'image' = ANY(args.allowed_embeds)
+                        AND COALESCE(cp.has_media, FALSE) = TRUE
+                    )
+                    OR (
+                        'video' = ANY(args.allowed_embeds)
+                        AND COALESCE(cp.has_video, FALSE) = TRUE
+                    )
+                )
+                -- Filter by NSFW status. If unspecified, do not filter.
+                AND (
+                    sqlc.narg(is_nsfw)::BOOLEAN IS NULL
+                    OR (
+                        (ARRAY['nsfw', 'mursuit', 'murrsuit', 'nsfwfurry', 'furrynsfw'] && cp.hashtags)
+                        OR (ARRAY['porn', 'nudity', 'sexual'] && cp.self_labels)
+                    ) = sqlc.narg(is_nsfw)
+                )
+            )
+            -- Pinned DID criteria.
+            OR cp.actor_did = ANY(sqlc.arg(pinned_dids)::TEXT [])
+        )
+        -- Remove posts newer than the cursor timestamp
+        AND (cp.indexed_at < sqlc.arg(cursor_timestamp))
+        AND cp.indexed_at > NOW() - INTERVAL '7 day'
+    ORDER BY
+        cp.indexed_at DESC
+    LIMIT sqlc.arg(_limit)
 )
 
-SELECT cp.*
-FROM
-    candidate_posts AS cp
-INNER JOIN candidate_actors AS ca ON cp.actor_did = ca.did
-NATURAL JOIN args
-WHERE
-    -- Only include posts by approved actors
-    ca.status = 'approved'
-    -- Remove posts hidden by our moderators
-    AND cp.is_hidden = FALSE
-    -- Remove posts deleted by the actors
-    AND cp.deleted_at IS NULL
-    AND (
-    -- Standard criteria.
-        (
-            -- Match at least one of the queried hashtags.
-            -- If unspecified, do not filter.
-            (
-                COALESCE(sqlc.narg(hashtags)::TEXT [], '{}') = '{}'
-                OR sqlc.arg(hashtags)::TEXT [] && cp.hashtags
-            )
-            -- If any hashtags are disallowed, filter them out.
-            AND (
-                COALESCE(sqlc.narg(disallowed_hashtags)::TEXT [], '{}') = '{}'
-                OR NOT sqlc.narg(disallowed_hashtags)::TEXT [] && cp.hashtags
-            )
-            AND (
-                CARDINALITY(args.allowed_embeds) = 0
-                OR (
-                    'none' = ANY(args.allowed_embeds)
-                    AND COALESCE(cp.has_media, FALSE) = FALSE
-                    AND COALESCE(cp.has_video, FALSE) = FALSE
-                )
-                OR (
-                    'image' = ANY(args.allowed_embeds)
-                    AND COALESCE(cp.has_media, FALSE) = TRUE
-                )
-                OR (
-                    'video' = ANY(args.allowed_embeds)
-                    AND COALESCE(cp.has_video, FALSE) = TRUE
-                )
-            )
-            -- Filter by NSFW status. If unspecified, do not filter.
-            AND (
-                sqlc.narg(is_nsfw)::BOOLEAN IS NULL
-                OR (
-                    (ARRAY['nsfw', 'mursuit', 'murrsuit', 'nsfwfurry', 'furrynsfw'] && cp.hashtags)
-                    OR (ARRAY['porn', 'nudity', 'sexual'] && cp.self_labels)
-                ) = sqlc.narg(is_nsfw)
-            )
-        )
-        -- Pinned DID criteria.
-        OR cp.actor_did = ANY(sqlc.arg(pinned_dids)::TEXT [])
-    )
-    -- Remove posts newer than the cursor timestamp
-    AND (cp.indexed_at < sqlc.arg(cursor_timestamp))
-    AND cp.indexed_at > NOW() - INTERVAL '7 day'
-    AND cp.created_at > NOW() - INTERVAL '7 day'
-ORDER BY
-    cp.indexed_at DESC
-LIMIT sqlc.arg(_limit);
+SELECT * FROM candidate_posts WHERE created_at > NOW() - INTERVAL '7 day';
 
 -- name: GetPostByURI :one
 SELECT *
@@ -100,64 +103,67 @@ LIMIT 1;
 -- name: ListScoredPosts :many
 WITH args AS (
     SELECT sqlc.narg(allowed_embeds)::TEXT [] AS allowed_embeds
+),
+
+candidate_posts AS (
+    SELECT
+        cp.*,
+        ph.score
+    FROM
+        candidate_posts AS cp
+    INNER JOIN candidate_actors AS ca ON cp.actor_did = ca.did
+    INNER JOIN post_scores AS ph
+        ON
+            cp.uri = ph.uri AND ph.alg = sqlc.arg(alg)
+            AND ph.generation_seq = sqlc.arg(generation_seq)
+    NATURAL JOIN args
+    WHERE
+        cp.is_hidden = FALSE
+        AND ca.status = 'approved'
+        -- Match at least one of the queried hashtags.
+        -- If unspecified, do not filter.
+        AND (
+            COALESCE(sqlc.narg(hashtags)::TEXT [], '{}') = '{}'
+            OR sqlc.narg(hashtags)::TEXT [] && cp.hashtags
+        )
+        -- If any hashtags are disallowed, filter them out.
+        AND (
+            COALESCE(sqlc.narg(disallowed_hashtags)::TEXT [], '{}') = '{}'
+            OR NOT sqlc.narg(disallowed_hashtags)::TEXT [] && cp.hashtags
+        )
+        AND (
+            CARDINALITY(args.allowed_embeds) = 0
+            OR (
+                'none' = ANY(args.allowed_embeds)
+                AND COALESCE(cp.has_media, FALSE) = FALSE
+                AND COALESCE(cp.has_video, FALSE) = FALSE
+            )
+            OR (
+                'image' = ANY(args.allowed_embeds)
+                AND COALESCE(cp.has_media, FALSE) = TRUE
+            )
+            OR (
+                'video' = ANY(args.allowed_embeds)
+                AND COALESCE(cp.has_video, FALSE) = TRUE
+            )
+        )
+        -- Filter by NSFW status. If unspecified, do not filter.
+        AND (
+            sqlc.narg(is_nsfw)::BOOLEAN IS NULL
+            OR (
+                (ARRAY['nsfw', 'mursuit', 'murrsuit', 'nsfwfurry', 'furrynsfw'] && cp.hashtags)
+                OR (ARRAY['porn', 'nudity', 'sexual'] && cp.self_labels)
+            ) = sqlc.narg(is_nsfw)
+        )
+        AND cp.deleted_at IS NULL
+        AND (
+            ROW(ph.score, ph.uri)
+            < ROW((sqlc.arg(after_score))::REAL, (sqlc.arg(after_uri))::TEXT)
+        )
+        AND cp.indexed_at > NOW() - INTERVAL '7 day'
+    ORDER BY
+        ph.score DESC, ph.uri DESC
+    LIMIT sqlc.arg(_limit)
 )
 
-SELECT
-    cp.*,
-    ph.score
-FROM
-    candidate_posts AS cp
-INNER JOIN candidate_actors AS ca ON cp.actor_did = ca.did
-INNER JOIN post_scores AS ph
-    ON
-        cp.uri = ph.uri AND ph.alg = sqlc.arg(alg)
-        AND ph.generation_seq = sqlc.arg(generation_seq)
-NATURAL JOIN args
-WHERE
-    cp.is_hidden = FALSE
-    AND ca.status = 'approved'
-    -- Match at least one of the queried hashtags.
-    -- If unspecified, do not filter.
-    AND (
-        COALESCE(sqlc.narg(hashtags)::TEXT [], '{}') = '{}'
-        OR sqlc.narg(hashtags)::TEXT [] && cp.hashtags
-    )
-    -- If any hashtags are disallowed, filter them out.
-    AND (
-        COALESCE(sqlc.narg(disallowed_hashtags)::TEXT [], '{}') = '{}'
-        OR NOT sqlc.narg(disallowed_hashtags)::TEXT [] && cp.hashtags
-    )
-    AND (
-        CARDINALITY(args.allowed_embeds) = 0
-        OR (
-            'none' = ANY(args.allowed_embeds)
-            AND COALESCE(cp.has_media, FALSE) = FALSE
-            AND COALESCE(cp.has_video, FALSE) = FALSE
-        )
-        OR (
-            'image' = ANY(args.allowed_embeds)
-            AND COALESCE(cp.has_media, FALSE) = TRUE
-        )
-        OR (
-            'video' = ANY(args.allowed_embeds)
-            AND COALESCE(cp.has_video, FALSE) = TRUE
-        )
-    )
-    -- Filter by NSFW status. If unspecified, do not filter.
-    AND (
-        sqlc.narg(is_nsfw)::BOOLEAN IS NULL
-        OR (
-            (ARRAY['nsfw', 'mursuit', 'murrsuit', 'nsfwfurry', 'furrynsfw'] && cp.hashtags)
-            OR (ARRAY['porn', 'nudity', 'sexual'] && cp.self_labels)
-        ) = sqlc.narg(is_nsfw)
-    )
-    AND cp.deleted_at IS NULL
-    AND (
-        ROW(ph.score, ph.uri)
-        < ROW((sqlc.arg(after_score))::REAL, (sqlc.arg(after_uri))::TEXT)
-    )
-    AND cp.indexed_at > NOW() - INTERVAL '7 day'
-    AND cp.created_at > NOW() - INTERVAL '7 day'
-ORDER BY
-    ph.score DESC, ph.uri DESC
-LIMIT sqlc.arg(_limit);
+SELECT * FROM candidate_posts WHERE created_at > NOW() - INTERVAL '7 day';
